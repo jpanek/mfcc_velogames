da1bcee121fb971468a96309e609eb2eb5bfeb65
Juraj <juraj.panek@gmail.com>
2026-01-23 12:27:03 +0100

    added year to race in navbar

--- hooks.py
+++ hooks.py
@@ -3,5 +3,5 @@ from utils.db_functions import get_data_from_db
 from datetime import datetime
 
 def before_request():
-    _, races = get_data_from_db("select name, race_id from races where 0=0 order by end_date desc")
+    _, races = get_data_from_db("select name||'-'||substr(year,-2) as name, race_id from races where 0=0 order by end_date desc")
     g.races = races
